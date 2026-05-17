afb60b4de253853c256cc907fa3415496fa252f4
geugenio <geugnio12@gmail.com>
2026-05-17 03:15:58 -0300

    feat: adicionar entidade de PerfilAnunciante

---
+++ backend/apto-api/src/main/java/com/apto/model/PerfilAnunciante.java
@@ -0,0 +1,28 @@
+package com.apto.model;
+
+import com.apto.model.entity.Usuario;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.UUID;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "perfis_anunciante")
+public class PerfilAnunciante {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(columnDefinition = "uuid", updatable = false, nullable = false)
+    private UUID id;
+
+    @OneToOne(optional = false)
+    @JoinColumn(name = "usuario_id", nullable = false, unique = true)
+    private Usuario usuario;
+
+    @Column(nullable = false)
+    private boolean ativo = true;
+}
